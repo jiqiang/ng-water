e76cfe2615b820769bac5c1be78509dd385a52fc
Glenn Ji <qiang.ji@yahoo.com.au>
2015-10-25 16:03:50 +1100

    Add title.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="e">
   <head>
       <meta charset="utf-8">
+      <title>BOM Water Information Dashboard</title>
       <!-- Latest compiled and minified CSS -->
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
