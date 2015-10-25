6cd5bc69cc78587316e0a0ea8eea8a54711763b2
Glenn Ji <qiang.ji@yahoo.com.au>
2015-10-25 20:26:09 +1100

    Add dellstore module.

---
+++ dellstore.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace DellStore;
+
+/**
+ * Dell store class.
+ */
+class DellStore
+{
+
+    private $_dbConn;
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Find customer order history.
+     *
+     * @return array
+     */
+    public function findCustomerOrderHistory()
+    {
+        return $this->connection->select(
+            'cust_hist',
+            array(
+                '[><]customers' => 'customerid',
+                '[><]products' => 'prod_id',
+                '[><]orders' => 'orderid',
+            ),
+            array(
+                'customers.firstname',
+                'customers.lastname',
+                'products.title',
+                'orders.orderdate',
+                'orders.totalamount',
+            ),
+            array(
+                'ORDER' => 'orders.orderdate ASC',
+                'LIMIT' => 100,
+            )
+        );
+    }
+
+    /**
+     * Get database connection.
+     *
+     * @param  resource $connection Medoo class instance.
+     *
+     * @return resource Medoo class instance.
+     */
+    public function getDatabaseConnection($connection)
+    {
+        $this->_dbConn = $connection;
+    }
+}
