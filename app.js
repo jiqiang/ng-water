f63d03cbd965912896d026b93c82d4ddc9200257
Glenn Ji <qiang.ji@yahoo.com.au>
2015-11-08 19:32:03 +1100

    Run jsLint.

--- app.js
+++ app.js
@@ -1,90 +1,91 @@
 /*global $, angular, _, console*/
+(function () {
+    'use strict';
 
-
-'use strict';
-
-$('.manual-toggle').click(function (e) {
-    e.stopPropagation();
-});
-
-var mainController = function ($scope, $location, $http) {
-
-
-    $http({
-        method: 'GET',
-        url: 'server.php',
-        params: [
-            {theme: 'landing', themeview: 'national'},
-            {theme: 'landing2', themeview: 'national2'}
-        ]
-    }).then(function (response) {
-        console.log(response.data);
+    $('.manual-toggle').click(function (e) {
+        e.stopPropagation();
     });
 
-    $scope.sortType = 'name';
-    $scope.sortReverse = false;
-    $scope.searchFish = '';
+    var mainController = function ($scope, $http) {
 
-    // Tasty level dropdown checkboxes.
-    $scope.globalCheckAll = {code: 'ALL', isChecked: true};
-    $scope.tastiLevels = [
-        {code: 1, isChecked: true},
-        {code: 2, isChecked: true},
-        {code: 3, isChecked: true},
-        {code: 4, isChecked: true},
-        {code: 5, isChecked: true},
-        {code: 6, isChecked: true},
-        {code: 7, isChecked: true},
-        {code: 8, isChecked: true}
-    ];
-    $scope.toggleCheckAll = function () {
-        $scope.tastiLevels.map(function (value, index) {
-            $scope.tastiLevels[index].isChecked = $scope.globalCheckAll.isChecked;
+
+        $http({
+            method: 'GET',
+            url: 'server.php',
+            params: [
+                {theme: 'landing', themeview: 'national'},
+                {theme: 'landing2', themeview: 'national2'}
+            ]
+        }).then(function (response) {
+            console.log(response.data);
         });
+
+        $scope.sortType = 'name';
+        $scope.sortReverse = false;
+        $scope.searchFish = '';
+
+        // Tasty level dropdown checkboxes.
+        $scope.globalCheckAll = {code: 'ALL', isChecked: true};
+        $scope.tastiLevels = [
+            {code: 1, isChecked: true},
+            {code: 2, isChecked: true},
+            {code: 3, isChecked: true},
+            {code: 4, isChecked: true},
+            {code: 5, isChecked: true},
+            {code: 6, isChecked: true},
+            {code: 7, isChecked: true},
+            {code: 8, isChecked: true}
+        ];
+        $scope.toggleCheckAll = function () {
+            $scope.tastiLevels.map(function (value, index) {
+                $scope.tastiLevels[index].isChecked = $scope.globalCheckAll.isChecked;
+            });
+        };
+
+        // Sushi roll dropdown checkboxes.
+        $scope.globalCheckAllSushiRolls = {code: 'ALL', isChecked: true};
+        $scope.sushiRolls = [
+            {code: 'Cali Roll', isChecked: true},
+            {code: 'Philly', isChecked: true},
+            {code: 'Tiger', isChecked: true},
+            {code: 'Rainbow', isChecked: true}
+        ];
+        $scope.toggleCheckAllSushiRolls = function () {
+            $scope.sushiRolls.map(function (value, index) {
+                $scope.sushiRolls[index].isChecked = $scope.globalCheckAllSushiRolls.isChecked;
+            });
+        };
+
+        // Fish type dropdown list.
+        $scope.fishTypes = [
+            {code: 'All'},
+            {code: 'Crab'},
+            {code: 'Tuna'},
+            {code: 'Eel'},
+            {code: 'Variety'}
+        ];
+        $scope.fishType = {code: 'All'};
+        $scope.updateSelectedFishType = function (ft) {
+            $scope.fishType = ft;
+        };
+
+        $scope.filterByTastiness = function (value) {
+            var found, foundSushiRoll;
+            found = _.findWhere($scope.tastiLevels, {code: value.tastiness, isChecked: true});
+            foundSushiRoll = _.findWhere($scope.sushiRolls, {code: value.name, isChecked: true});
+
+            return found !== undefined && foundSushiRoll !== undefined && ($scope.fishType.code === 'All' || $scope.fishType.code === value.fish) ? true : false;
+        };
+
+        $scope.sushi = [
+            {name: 'Cali Roll', fish: 'Crab', tastiness: 2, show: true},
+            {name: 'Philly', fish: 'Tuna', tastiness: 4, show: true},
+            {name: 'Tiger', fish: 'Eel', tastiness: 7, show: true},
+            {name: 'Rainbow', fish: 'Variety', tastiness: 6, show: true}
+        ];
     };
 
-    // Sushi roll dropdown checkboxes.
-    $scope.globalCheckAllSushiRolls = {code: 'ALL', isChecked: true};
-    $scope.sushiRolls = [
-        {code: 'Cali Roll', isChecked: true},
-        {code: 'Philly', isChecked: true},
-        {code: 'Tiger', isChecked: true},
-        {code: 'Rainbow', isChecked: true}
-    ];
-    $scope.toggleCheckAllSushiRolls = function () {
-        $scope.sushiRolls.map(function (value, index) {
-            $scope.sushiRolls[index].isChecked = $scope.globalCheckAllSushiRolls.isChecked;
-        });
-    };
+    angular.module('sortApp', []).controller('mainController', mainController);
 
-    // Fish type dropdown list.
-    $scope.fishTypes = [
-        {code: 'All'},
-        {code: 'Crab'},
-        {code: 'Tuna'},
-        {code: 'Eel'},
-        {code: 'Variety'}
-    ];
-    $scope.fishType = {code: 'All'};
-    $scope.updateSelectedFishType = function (ft) {
-        $scope.fishType = ft;
-    };
-
-    $scope.filterByTastiness = function (value) {
-        var found, foundSushiRoll;
-        found = _.findWhere($scope.tastiLevels, {code: value.tastiness, isChecked: true});
-        foundSushiRoll = _.findWhere($scope.sushiRolls, {code: value.name, isChecked: true});
-
-        return found !== undefined && foundSushiRoll !== undefined && ($scope.fishType.code === 'All' || $scope.fishType.code === value.fish) ? true : false;
-    };
-
-    $scope.sushi = [
-        {name: 'Cali Roll', fish: 'Crab', tastiness: 2, show: true},
-        {name: 'Philly', fish: 'Tuna', tastiness: 4, show: true},
-        {name: 'Tiger', fish: 'Eel', tastiness: 7, show: true},
-        {name: 'Rainbow', fish: 'Variety', tastiness: 6, show: true}
-    ];
-};
-
-angular.module('sortApp', []).controller('mainController', mainController);
+}());
 
