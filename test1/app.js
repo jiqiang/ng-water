6b3859e9430441affd83e14cd2df2696f0d88434
Glenn Ji <qiang.ji@yahoo.com.au>
2015-11-12 01:06:40 +1100

    Build directive.

--- test1/app.js
+++ test1/app.js
@@ -1,18 +1,30 @@
 angular.module('myApp', [])
 
 .controller('mainController', ['$scope', function($scope) {
-
+    $scope.dcoptions = ['abc', 'bcd', 'cde', 'def'];
+    $scope.dcoptions2 = ['abc2', 'bcd2', 'cde2', 'def2'];
 }])
 .directive('widDropdownCheckbox', function() {
     return {
         restrict: 'EA',
         transclude: false,
         //templateUrl: 'wid-dropdown.tpl.html',
-        scope: {
-
-        },
-        link: function (scope, element, attr) {
-            element.multiselect().multiselectfilter();
+        replace: false,
+        scope: { options: '=dcOptions' },
+        link: function (scope, element, attrs) {
+            angular.forEach(scope.options, function (value, key) {
+                $('<option />', {value: value, text: value, selected: true}).appendTo(element);
+            });
+            element.css({width: "370px"});
+            var config = scope.$eval(attrs.widDropdownCheckbox);
+            element.multiselect({
+                noneSelectedText: config.label,
+                selectedText: config.label,
+                click: function (event, ui) {
+                    console.log(ui);
+                }
+            })
+            .multiselectfilter();
         }
     };
 });
